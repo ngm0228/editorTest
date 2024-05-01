e00ed4d48cf8f5fff55f21b8e6e38e19d16420cb
ngm0228 <ngm0228@gmail.com>
2024-05-01 18:07:58 +0900

    IMG 삽입 수정해야함

--- react-quillTest/src/MyEditor.jsx
+++ react-quillTest/src/MyEditor.jsx
@@ -1,24 +1,32 @@
 import React, { useMemo, useRef, useState } from 'react';
 import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css'; // Snow 테마 사용
+import 'react-quill/dist/quill.snow.css';
+import { editorTest } from './apis/test';
 
 function MyEditor() {
   const [content, setContent] = useState('');
   const quillRef = useRef();
+  const [test, setTest] = useState(true);
+
 
   const imageHandler = () => {
     console.log("이미지 핸들러 작동");
+    console.log(content);
     const input = document.createElement('input');
     input.setAttribute('type', 'file');
     input.setAttribute('accept', 'image/*');
     input.click();
-
     input.onchange = async () => {
       const file = input.files[0];
       const formData = new FormData();
       formData.append('image', file);
 
-      console.log(formData.get('image'));
+      let imgUrl = "<img src='../public/tempImg/" + file.name + "'/>"
+      console.log(imgUrl);
+      
+      setContent(imgUrl);
+
+      editorTest(formData);
     }
   }
 
@@ -26,22 +34,25 @@ function MyEditor() {
     return {
       toolbar: {
         container: [
-          [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
-          [{size: []}],
+          [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+          [{ size: [] }],
           ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-          [{'list': 'ordered'}, {'list': 'bullet'}, 
-          {'indent': '-1'}, {'indent': '+1'}],
+          [{ 'list': 'ordered' }, { 'list': 'bullet' },
+          { 'indent': '-1' }, { 'indent': '+1' }],
           ['link', 'image', 'video'],
           ['clean']
         ],
         handlers: {
           image: imageHandler
-        }, 
+        },
       },
     }
   }, [])
 
-  
+  const onClickTest = ()=>{
+    setTest(test? false : true);
+    console.log(test);
+  }
 
   const formats = [
     'header', 'font', 'size',
@@ -56,14 +67,18 @@ function MyEditor() {
   };
 
   return (
-    <ReactQuill 
-      theme="snow" 
-      modules={modules}
-      formats={formats}
-      value={content} 
-      onChange={handleChange}
-      ref={quillRef}
-    />
+    <>
+    <img src="../public/tempImg/images.jpg" alt="test1" />
+      <ReactQuill
+        theme="snow"
+        modules={modules}
+        formats={formats}
+        value={content}
+        onChange={handleChange}
+        ref={quillRef}
+      />
+      <button onClick={()=>onClickTest()}>버튼이다</button>
+    </>
   );
 }
 
--- react-quillTest/src/apis/test.js
+++ react-quillTest/src/apis/test.js
@@ -3,9 +3,9 @@ import { imgAPI } from "../core";
 
 export const editorTest = async(params) => {
     try {
-        const {data} = await imgAPI.post("/test", params);
+        const {data} = await imgAPI.post("/user/test", params);
         return data;
     } catch (e){
-        
+        console.log(e);
     }
 }
